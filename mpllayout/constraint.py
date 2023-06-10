136d1e62370b5be3c030860c9380ba40c0680f67
Jonathan Deng <jonathan.j.deng@gmail.com>
2023-06-10 14:12:53 -0400

    wip: Add simple constraint solver
    Constraints were refactored to be functions.
    
    A constrained geometric problem is now created by creating a graph that
    links constraints to primitives.

--- mpllayout/constraint.py
+++ mpllayout/constraint.py
@@ -1,56 +1,107 @@
 
-from typing import Optional, List
+"""
+Geometric constraints
+"""
+
+from typing import Optional, List, Tuple, Type
 from numpy.typing import NDArray
 
 import numpy as np
 
+import jax
+import jax.numpy as jnp
+
 from .primitive import Primitive, Point
 
-def solve(constraints: List['Constraint']):
-    # Build a mapping for each primitive to it's associated constraints
-    prim_to_constraints = {}
-    for constraint in constraints:
-        for prim in constraint.prims:
-            if prim in prim_to_constraints:
-                prim_to_constraints[prim].append(constraint)
-            else:
-                prim_to_constraints[prim] = [constraint]
+Prims = Tuple[Primitive, ...]
 
-    # Check that each primitive has a set of complete constraints
-    for prim, constraints in prim_to_constraints.items():
-        if isinstance(prim, Point):
-            assert len(constraints) == 2
-
-            assert not np.isclose(
-                np.dot(constraints[0].dir, constraints[1].dir), 0
-            )
-
-
+Idxs = Tuple[int]
 
 
 class Constraint:
+    """
+    Constraint base class
+    """
     
-    def __init__(self, prims: List[Primitive]):
-        self._prims = tuple(prims)
+    primitive_types: Tuple[Type[Primitive], ...]
 
-    @property
-    def prims(self):
-        return self._prims
+    def __call__(self, prims: Tuple[Primitive, ...]):
+        # Check the input primitives are valid
+        assert len(prims) == len(self.primitive_types)
+        for prim, prim_type in zip(prims, self.primitive_types):
+            assert issubclass(type(prim), prim_type)
+
+        return jnp.atleast_1d(self.assem_res(prims))
+    
+    def assem_res(self, prims: Tuple[Primitive, ...]) -> NDArray:
+        raise NotImplementedError()
 
 class PointToPointAbsDistance(Constraint):
 
+    primitive_types = (Point, Point)
+
     def __init__(
-            self, pointa: Point, pointb: Point, 
-            distance: float, dir: Optional[NDArray]=None
+            self, distance: float, direction: Optional[NDArray]=None
         ):
-        super().__init__((pointa, pointb))
+        if direction is None:
+            direction = np.array([1, 0])
+        else:
+            direction = np.array(direction)
 
-        if dir is None:
-            dir = np.array([1, 0])
-
-        self._dir = dir
+        self._direction = direction
         self._distance = distance
 
     @property
     def distance(self):
         return self._distance
+    
+    @property
+    def direction(self):
+        return self._direction
+
+    def assem_res(self, prims):
+        return jnp.dot(prims[1].param - prims[0].param, self.direction) - self.distance
+
+
+ConstraintGraph = List[Tuple[int, Idxs]]
+
+def solve(
+        constraints: List[Constraint], 
+        prims: List[Primitive], 
+        constraint_graph: ConstraintGraph
+    ):
+
+    prim_sizes = [prim.param.size for prim in prims]
+    prim_global_idx_bounds = np.cumsum([0] + prim_sizes)
+
+    global_param_n = np.concatenate([prim.param for prim in prims])
+
+    def assem_global_res(global_param):
+        constraint_vals = []
+        for (constraint_idx, prim_idxs) in constraint_graph:
+            constraint = constraints[constraint_idx]
+            params = tuple(
+                global_param[
+                    prim_global_idx_bounds[idx]:prim_global_idx_bounds[idx+1]
+                ] 
+                for idx in prim_idxs
+            )
+            local_prims = tuple(
+                Prim(param) for Prim, param in zip(constraint.primitive_types, params)
+            )
+
+            constraint_vals.append(constraint(local_prims))
+        return jnp.concatenate(constraint_vals)
+        
+    global_res = assem_global_res(global_param_n)
+    global_jac = jax.jacfwd(assem_global_res)(global_param_n)
+
+    dglobal_param, err, rank, s = np.linalg.lstsq(global_jac, -global_res, rcond=None)
+    global_param_n = global_param_n + dglobal_param
+
+    new_prim_params = [
+        np.array(global_param_n[idx_start:idx_end])
+        for idx_start, idx_end in zip(prim_global_idx_bounds[:-1], prim_global_idx_bounds[1:])
+    ]
+
+    return new_prim_params
--- mpllayout/primitive.py
+++ mpllayout/primitive.py
@@ -1,19 +1,42 @@
-
+"""
+Geometric primitives
+"""
 
 from numpy.typing import NDArray
 import numpy as np
+from jax import numpy as jnp
 
 class Primitive:
-    pass
+    """
+    Geometric primitive base class
+
+    Parameters
+    ----------
+    param: ArrayLike with shape (n,)
+        A parameter vector for the primitive
+    """
+
+    def __init__(self, param: NDArray):
+
+        # Parameter vector for the primitive
+        if not isinstance(param, (np.ndarray, jnp.ndarray)):
+            param = np.array(param)
+        
+        self._param: NDArray = param
+
+    @property
+    def param(self):
+        """
+        Return the primitive's parameter vector
+        """
+        return self._param
 
 class Point(Primitive):
 
-    def __init__(self, coords: NDArray):
-        self._coords = np.array(coords)
+    def __init__(self, param: NDArray):
+        super().__init__(param)
 
-    @property
-    def coords(self):
-        return self._coords
+        assert self.param.shape == (2,)
     
     def __repr__(self):
-        return f'Point({self.coords})'
+        return f'Point({self.param})'
