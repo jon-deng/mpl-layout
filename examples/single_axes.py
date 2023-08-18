43fa4ee82eaac1f5938ae2c6dd89a8aa6e9ec357
Jonathan Deng <jonathan.j.deng@gmail.com>
2023-08-18 16:02:16 -0400

    misc: Add an example of single axes creation with the `ClosedPolyline`

--- examples/single_axes.py
+++ examples/single_axes.py
@@ -1,5 +1,5 @@
 """
-Create a single axes from a `bbox`
+Create a single axes figure
 """
 
 import numpy as np
@@ -10,71 +10,78 @@ from mpllayout import solver, geometry as geo
 if __name__ == '__main__':
     layout = solver.Layout()
 
-    # Create an origin point and the figure max extent point
+    # Create an origin point
     layout.add_prim(geo.Point([0, 0]), 'Origin')
     layout.add_constraint(geo.PointLocation(np.array([0, 0])), ('Origin',))
 
-    layout.add_prim(geo.Point([0, 0]), 'TopRightCorner')
-    layout.add_constraint(geo.PointLocation(np.array([5, 5])), ('TopRightCorner',))
+    # Create the figure box
+    verts = np.array([
+        [0, 0], [5, 0], [5, 5], [0, 5]
+    ])
+    box = geo.ClosedPolyline(
+        prims=[geo.Point(vert_coords) for vert_coords in verts]
+    )
+    layout.add_prim(box, 'Figure')
 
-    # Create a box representing the bbox for the axes
-    xmin, xmax = 1, 4
-    ymin, ymax = 1, 2
-    box_points = [
-        geo.Point([xmin+0.1, ymin+0.5]),
-        geo.Point([xmax+0.6, ymin]),
-        geo.Point([xmax, ymax]),
-        geo.Point([xmin-0.1, ymax])
-    ]
-    box_lines = [
-        geo.LineSegment(prims=(pointa, pointb))
-        for pointa, pointb in zip(box_points, box_points[1:]+box_points[:1])
-    ]
-    layout.add_prim(geo.Box(prims=box_lines), 'Axes1')
+    layout.add_constraint(
+        geo.CoincidentPoint(),
+        ('Figure.Point0', 'Origin')
+    )
 
-    # Constrain the margins of the axes
+    # Create the axes box
+    verts = np.array([
+        [0, 0], [5, 0], [5, 5], [0, 5]
+    ])
+    box = geo.ClosedPolyline(
+        prims=[geo.Point(vert_coords) for vert_coords in verts]
+    )
+    layout.add_prim(box, 'Axes1')
+
+    # Constrain 'Axes1' margins
     margin_x = 1.1
     margin_top = 1.1
     margin_bottom = 0.5
     layout.add_constraint(
         geo.PointToPointAbsDistance(margin_x, np.array([-1, 0])),
-        ('Axes1.LineSegment0.Point0', 'Origin')
+        ('Axes1.Point0', 'Figure.Point0')
     )
     layout.add_constraint(
         geo.PointToPointAbsDistance(margin_x, np.array([1, 0])),
-        ('Axes1.LineSegment0.Point1', 'TopRightCorner')
-    )
-
-    layout.add_constraint(
-        geo.PointToPointAbsDistance(margin_bottom, np.array([0, -1])),
-        ('Axes1.LineSegment0.Point0', 'Origin')
-    )
-    layout.add_constraint(
-        geo.PointToPointAbsDistance(margin_top, np.array([0, 1])),
-        ('Axes1.LineSegment1.Point1', 'TopRightCorner')
+        ('Axes1.Point2', 'Figure.Point2')
     )
 
     prims, info = solver.solve(
         layout.prims, layout.constraints, layout.constraint_graph
     )
 
-    width = prims['TopRightCorner'].param[0] - prims['Origin'].param[0]
-    height = prims['TopRightCorner'].param[1] - prims['Origin'].param[1]
-
     print(prims['Axes1'])
+
+    def wh_from_box(box: geo.Box):
+
+        point_bottomleft = box.prims[0]
+        xmin = point_bottomleft.param[0]
+        ymin = point_bottomleft.param[1]
+
+        point_topright = box.prims[2]
+        xmax = point_topright.param[0]
+        ymax = point_topright.param[1]
+
+        return (xmax-xmin), (ymax-ymin)
 
     def rect_from_box(box: geo.Box, fig_size=(1, 1)):
         fig_w, fig_h = fig_size
 
-        point_bottomleft = box.prims[0].prims[0]
+        point_bottomleft = box.prims[0]
         xmin = point_bottomleft.param[0]/fig_w
         ymin = point_bottomleft.param[1]/fig_h
 
-        point_topright = box.prims[1].prims[1]
+        point_topright = box.prims[1]
         xmax = point_topright.param[0]/fig_w
         ymax = point_topright.param[1]/fig_h
 
         return (xmin, ymin, (xmax-xmin), (ymax-ymin))
+    
+    width, height = wh_from_box(prims['Figure'])
 
     print(rect_from_box(prims['Axes1'], fig_size=(width, height)))
 
