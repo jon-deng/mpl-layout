ee130308c0482917aa2ba4b93d58572547e501f0
Jonathan Deng <jonathan.j.deng@gmail.com>
2023-08-22 14:30:59 -0400

    example: Fix single axes example

--- examples/single_axes.py
+++ examples/single_axes.py
@@ -7,68 +7,81 @@ from matplotlib import pyplot as plt
 
 from mpllayout import solver, geometry as geo
 
+PrimIdx = geo.PrimIdx
+
 if __name__ == '__main__':
     layout = solver.Layout()
 
     ## Create an origin point
     layout.add_prim(geo.Point([0, 0]), 'Origin')
-    layout.add_constraint(geo.PointLocation(np.array([0, 0])), ('Origin',))
+    layout.add_constraint(geo.PointLocation(np.array([0, 0])), (PrimIdx('Origin'),))
 
-    ## Create the Figure box
-    verts = np.array([
+    ## Create the figure box
+    verts = [
         [0, 0], [5, 0], [5, 5], [0, 5]
-    ])
-    box = geo.ClosedPolyline(
+    ]
+    box = geo.Box(
         prims=[geo.Point(vert_coords) for vert_coords in verts]
     )
     layout.add_prim(box, 'Figure')
 
     layout.add_constraint(
         geo.CoincidentPoint(),
-        ('Figure.Point0', 'Origin')
+        (PrimIdx('Figure.Point0'), PrimIdx('Origin'))
     )
-
-    # Make the box square
-    layout.add_constraint(
-        geo.Horizontal(), ('Figure',), (0,)
-    )
-    # layout.add_constraint(
-    #     geo.Vertical(), ('Figure',), (1,)
-    # )
-    # layout.add_constraint(
-    #     geo.Horizontal(), ('Figure',), (2,)
-    # )
-    # layout.add_constraint(
-    #     geo.Vertical(), ('Figure',), (3,)
-    # )
 
     ## Create the axes box
-    verts = np.array([
+    verts = [
         [0, 0], [5, 0], [5, 5], [0, 5]
-    ])
-    box = geo.ClosedPolyline(
+    ]
+    box = geo.Box(
         prims=[geo.Point(vert_coords) for vert_coords in verts]
     )
     layout.add_prim(box, 'Axes1')
 
-    # Constrain 'Axes1' margins
-    margin_x = 1.1
+    ## Constrain the figure size
+    fig_width, fig_height = 6, 3
+    layout.add_constraint(
+        geo.PointToPointAbsDistance(fig_width, np.array([1, 0])),
+        (PrimIdx('Figure.Point0'), PrimIdx('Figure.Point1'))
+    )
+    layout.add_constraint(
+        geo.PointToPointAbsDistance(fig_height, np.array([0, 1])),
+        (PrimIdx('Figure.Point0'), PrimIdx('Figure.Point3'))
+    )
+
+    ## Constrain 'Axes1' margins
+    # Constrain left/right margins
+    margin_left = 1.1
+    margin_right = 1.1
+    layout.add_constraint(
+        geo.PointToPointAbsDistance(margin_left, np.array([-1, 0])),
+        (PrimIdx('Axes1.Point0'), PrimIdx('Figure.Point0'))
+    )
+    layout.add_constraint(
+        geo.PointToPointAbsDistance(margin_right, np.array([1, 0])),
+        (PrimIdx('Axes1.Point2'), PrimIdx('Figure.Point2'))
+    )
+
+    # Constrain top/bottom margins
     margin_top = 1.1
     margin_bottom = 0.5
     layout.add_constraint(
-        geo.PointToPointAbsDistance(margin_x, np.array([-1, 0])),
-        ('Axes1.Point0', 'Figure.Point0')
+        geo.PointToPointAbsDistance(margin_bottom, np.array([0, -1])),
+        (PrimIdx('Axes1.Point0'), PrimIdx('Figure.Point0'))
     )
     layout.add_constraint(
-        geo.PointToPointAbsDistance(margin_x, np.array([1, 0])),
-        ('Axes1.Point2', 'Figure.Point2')
+        geo.PointToPointAbsDistance(margin_top, np.array([0, 1])),
+        (PrimIdx('Axes1.Point2'), PrimIdx('Figure.Point2'))
     )
 
+    ## Solve the constraints and form the figure/axes layout
     prims, info = solver.solve(
         layout.prims, layout.constraints, layout.constraint_graph
     )
 
-    print(prims['Axes1'])
+    print('Figure:', prims['Figure'])
+    print('Axes1:', prims['Axes1'])
 
     def wh_from_box(box: geo.Box):
 
