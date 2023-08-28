07730e740bb79243405cc76336a007c6989666cd
Jonathan Deng <jonathan.j.deng@gmail.com>
2023-08-28 14:42:50 -0400

    docs: Improve `ui` documentation

--- mpllayout/ui.py
+++ mpllayout/ui.py
@@ -1,5 +1,5 @@
 """
-Utilities for a user interface/visualization of plot layout
+Utilities for a user interface/visualization of the plot layout
 """
 
 import typing as typ
@@ -10,17 +10,23 @@ import numpy as np
 from . import geometry as geo
 from .array import LabelledList
 
-## Function for plotting geometric primitives
+## Functions for plotting geometric primitives
 
 def plot_point(
         ax: mpl.axes.Axes, point: geo.Point, **kwargs
     ):
+    """
+    Plot a `Point` primitive to an axes
+    """
     x, y = point.param
     ax.plot([x], [y], **kwargs)
 
 def plot_line_segment(
         ax: mpl.axes.Axes, line_segment: geo.LineSegment, **kwargs
     ):
+    """
+    Plot a `LineSegment` primitive in an axes
+    """
     xs = np.array([point.param[0] for point in line_segment.prims])
     ys = np.array([point.param[1] for point in line_segment.prims])
     ax.plot(xs, ys, **kwargs)
@@ -28,14 +34,21 @@ def plot_line_segment(
 def plot_closed_polyline(
         ax: mpl.axes.Axes, polyline: geo.ClosedPolyline, **kwargs
     ):
+    """
+    Plot a `ClosedPolyline` primitive in an axes
+    """
     closed_prims = polyline.prims[:]+(polyline.prims[0],)
     xs = np.array([point.param[0] for point in closed_prims])
     ys = np.array([point.param[1] for point in closed_prims])
     ax.plot(xs, ys, **kwargs)
 
+## Functions for plotting arbitrary geometric primitives
 def make_plot(
         prim: geo.Primitive
     ) -> typ.Callable[[mpl.axes.Axes, typ.Tuple[geo.Primitive, ...]], None]:
+    """
+    Return a function that can plot a `geo.Primitive` object
+    """
 
     if isinstance(prim, geo.Point):
         return plot_point
@@ -49,6 +62,9 @@ def make_plot(
 def plot_prims(
         ax: mpl.axes.Axes, prims: LabelledList[geo.Primitive]
     ):
+    """
+    Plot a collection of `geo.Primitive` objects
+    """
 
     for prim in prims:
         plot = make_plot(prim)
