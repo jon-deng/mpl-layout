8cc4d65cec9ebb67e658a51e71a7909a9b3b176b
Jonathan Deng <jonathan.j.deng@gmail.com>
2023-08-28 14:35:58 -0400

    docs: Improve docstrings for `matplotlibutils`

--- mpllayout/matplotlibutils.py
+++ mpllayout/matplotlibutils.py
@@ -1,5 +1,5 @@
 """
-Create a single axes figure
+Utilities for creating `matplotlib` plot objects
 """
 
 import typing as typ
@@ -14,10 +14,24 @@ from mpllayout.array import LabelledList
 def subplots(
         prims: LabelledList[geo.Primitive]
     ) -> typ.Tuple[Figure, typ.Mapping[str, Axes]]:
+    """
+    Create `Figure` and `Axes` objects from geometric primitives
+
+    Parameters
+    ----------
+    prims: LabelledList[geo.Primitive]
+        A list of `Primitive` objects.
+        `Figure` and `Axes` objects are created from objects prefixed with
+        'Figure' or 'Axes'.
+
+    Returns
+    -------
+    fig, axs: typ.Tuple[Figure, typ.Mapping[str, Axes]]
+        A `Figure` instance and a mapping from axes labels to `Axes` instances
+        using the `Axes` object names.
+    """
 
     width, height = wh_from_box(prims['Figure'])
-
-    print(rect_from_box(prims['Axes1'], fig_size=(width, height)))
 
     fig = plt.Figure((width, height))
     axs = {
@@ -27,9 +41,18 @@ def subplots(
     }
     return fig, axs
 
-def wh_from_box(box: geo.Box):
+def wh_from_box(box: geo.Box) -> typ.Tuple[float, float]:
     """
     Return the width and height of a `Box`
+
+    Parameters
+    ----------
+    box: geo.Box
+
+    Returns
+    -------
+    typ.Tuple[float, float]
+        The `(width, height)` of the box
     """
 
     point_bottomleft = box.prims[0]
@@ -42,9 +65,26 @@ def wh_from_box(box: geo.Box):
 
     return (xmax-xmin), (ymax-ymin)
 
-def rect_from_box(box: geo.Box, fig_size=(1, 1)):
+def rect_from_box(
+        box: geo.Box, 
+        fig_size: typ.Optional[typ.Tuple[float, float]]=(1, 1)
+    ) -> typ.Tuple[float, float, float, float]:
     """
-    Return the lower left corner and width and height of a `Box`
+    Return the lower left corner coordinate and width and height of a `Box`
+
+    This tuple of box information can be used to create a `Bbox` object from
+    `matplotlib` or an `Axes`.
+
+    Parameters
+    ----------
+    box: geo.Box    
+        The box
+    fig_size: typ.Optional[typ.Tuple[float, float]]
+        The width and height of the figure
+
+    Returns
+    -------
+    xmin, ymin, width, heigth: typ.Tuple[float, float, float, float]
     """
     fig_w, fig_h = fig_size
 
@@ -55,5 +95,7 @@ def rect_from_box(box: geo.Box, fig_size=(1, 1)):
     point_topright = box.prims[2]
     xmax = point_topright.param[0]/fig_w
     ymax = point_topright.param[1]/fig_h
+    width = xmax-xmin
+    height = ymax-ymin
 
-    return (xmin, ymin, (xmax-xmin), (ymax-ymin))
+    return (xmin, ymin, width, height)
