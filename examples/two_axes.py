2eeb07d963672898c8d07a1115428b711bc34fc8
Jonathan Deng <jonathan.j.deng@gmail.com>
2023-08-29 13:07:54 -0400

    misc: Improve two_axes example to show more steps

--- examples/two_axes.py
+++ examples/two_axes.py
@@ -19,9 +19,14 @@ def plot_layout(layout: solver.Layout, fig_path: str):
 
     fig, ax = plt.subplots(1, 1)
 
-    ax.set_xlim(-1, 7)
-    ax.set_ylim(-1, 4)
+    ax.set_xlim(-1, 10)
+    ax.set_ylim(-1, 10)
+    ax.set_xticks(np.arange(-1, 11, 1))
+    ax.set_yticks(np.arange(-1, 11, 1))
     ax.set_aspect(1)
+
+    ax.set_xlabel("x [in]")
+    ax.set_ylabel("y [in]")
     ui.plot_prims(ax, array.LabelledList(root_prims, root_prim_labels))
 
     fig.savefig(fig_path)
@@ -36,7 +41,7 @@ if __name__ == '__main__':
     layout.add_prim(geo.Point([0, 0]), 'Origin')
     # Constrain the origin to be at (0, 0)
     layout.add_constraint(
-        geo.PointLocation(np.array([0, 0])), 
+        geo.PointLocation(np.array([0, 0])),
         (PrimIdx('Origin'),)
     )
 
@@ -47,7 +52,7 @@ if __name__ == '__main__':
     verts = [[0, 0], [5, 0], [5, 5], [0, 5]]
     # Create the box with an initial size of 5 by 5 and call it 'Figure'
     layout.add_prim(
-        geo.Box(prims=[geo.Point(vert) for vert in verts]), 
+        geo.Box(prims=[geo.Point(vert) for vert in verts]),
         'Figure'
     )
 
@@ -55,10 +60,10 @@ if __name__ == '__main__':
 
     ## Create another box to represent the left axes
 
-    verts = [[0, 0], [5, 0], [5, 5], [0, 5]]
+    verts = [[1, 1], [4, 1], [4, 4], [1, 4]]
     # Call the box 'Axes1'
     layout.add_prim(
-        geo.Box(prims=[geo.Point(vert) for vert in verts]), 
+        geo.Box(prims=[geo.Point(vert) for vert in verts]),
         'Axes1'
     )
 
@@ -66,10 +71,10 @@ if __name__ == '__main__':
 
     ## Create another box to represent the right axes
 
-    verts = [[0, 0], [5, 0], [5, 5], [0, 5]]
+    verts = [[2, 2], [5, 2], [5, 5], [2, 5]]
     # Call the box 'Axes2'
     layout.add_prim(
-        geo.Box(prims=[geo.Point(vert) for vert in verts]), 
+        geo.Box(prims=[geo.Point(vert) for vert in verts]),
         'Axes2'
     )
 
@@ -78,21 +83,21 @@ if __name__ == '__main__':
     ## Constrain the figure size
     fig_width, fig_height = 6, 3
 
-    # Constrain the bottom edge of the figure box (represented by 
+    # Constrain the bottom edge of the figure box (represented by
     # `PrimIdx('Figure', 0)`) to have length `fig_width`
     layout.add_constraint(
         geo.LineLength(fig_width),
         (PrimIdx('Figure', 0),)
     )
 
-    # Constrain the right edge of the figure box (represented by 
+    # Constrain the right edge of the figure box (represented by
     # `PrimIdx('Figure', 1)`) to have length `fig_height`
     layout.add_constraint(
         geo.LineLength(fig_height),
         (PrimIdx('Figure', 1),)
     )
 
-    # Constrain the bottom corner point of the figure box (`PrimIdx('Figure.Point0')`) 
+    # Constrain the bottom corner point of the figure box (`PrimIdx('Figure.Point0')`)
     # to be coincident with the origin (`PrimIdx('Origin')`)
     layout.add_constraint(
         geo.CoincidentPoint(),
@@ -101,39 +106,46 @@ if __name__ == '__main__':
 
     plot_layout(layout, 'out/2Axes--4.png')
 
-    ## Constrain the position and size of the left and right axes
-    
-    margin_left = 2.1
-    margin_right = 1.1
+    ## Constrain the left margin to `Axes1`
 
-    # Constrain the left margin to `Axes1`
+    margin_left = 0.5
     layout.add_constraint(
         geo.PointToPointAbsDistance(margin_left, np.array([-1, 0])),
         (PrimIdx('Axes1.Point0'), PrimIdx('Figure.Point0'))
     )
 
-    # Constrain the right margin to `Axes2`
+    plot_layout(layout, 'out/2Axes--5.png')
+
+    ## Constrain the right margin to `Axes2`
+
+    margin_right = 0.5
     layout.add_constraint(
         geo.PointToPointAbsDistance(margin_right, np.array([1, 0])),
         (PrimIdx('Axes2.Point1'), PrimIdx('Figure.Point2'))
     )
 
-    # Constrain the width of 'Axes1' by setting the length of the bottom edge
-    width = 0.5
+    plot_layout(layout, 'out/2Axes--6.png')
+
+    ## Constrain the width of 'Axes1' by setting the length of the bottom edge
+    width = 2
     layout.add_constraint(
         geo.LineLength(width),
         (PrimIdx('Axes1', 0),)
     )
 
-    # Constrain the gap between the left and right axes ('Axes1' and `Axes2`)
+    plot_layout(layout, 'out/2Axes--7.png')
+
+    ## Constrain the gap between the left and right axes ('Axes1' and `Axes2`)
     margin_inter = 0.5
     layout.add_constraint(
         geo.PointToPointAbsDistance(margin_inter, np.array([1, 0])),
         (PrimIdx('Axes1.Point1'), PrimIdx('Axes2.Point0'))
     )
 
-    # Constrain the top/bottom margins on the left axes ('Axes1')
-    margin_top = 1.1
+    plot_layout(layout, 'out/2Axes--8.png')
+
+    ## Constrain the top/bottom margins on the left axes ('Axes1')
+    margin_top = 1.0
     margin_bottom = 0.5
     layout.add_constraint(
         geo.PointToPointAbsDistance(margin_bottom, np.array([0, -1])),
@@ -144,7 +156,7 @@ if __name__ == '__main__':
         (PrimIdx('Axes1.Point2'), PrimIdx('Figure.Point2'))
     )
 
-    plot_layout(layout, 'out/2Axes--5.png')
+    plot_layout(layout, 'out/2Axes--9.png')
 
     ## Make the top/bottom edges of the right axes ('Axes2') line up with the
     # top/bottom edges of the left axes ('Axes1')
@@ -157,7 +169,7 @@ if __name__ == '__main__':
         (PrimIdx('Axes1', 2), PrimIdx('Axes2', 2))
     )
 
-    plot_layout(layout, 'out/2Axes--6.png')
+    plot_layout(layout, 'out/2Axes--10.png')
 
     ## Solve for the constrained positions of the primitives
     prims, info = solver.solve(
