7f4970d4dd9f814d2f705267bf8c8bfdc056833d
Jonathan Deng <jonathan.j.deng@gmail.com>
2023-12-24 13:27:46 -0800

    docs: Add better docstrings for `matplotlibutils`

--- mpllayout/matplotlibutils.py
+++ mpllayout/matplotlibutils.py
@@ -1,5 +1,5 @@
 """
-Utilities for creating `matplotlib` plot objects
+Utilities for creating `matplotlib` plot objects from primitives
 """
 
 import typing as typ
@@ -19,22 +19,22 @@ def subplots(
 
     The `Figure` and `Axes` objects are extracted based on labels in `prims`.
     A `geo.Box` primitive named 'Figure' is used to create the `Figure` with
-    corresponding dimensions.
-    Any `geo.Box` primitives prefixed with 'Axes' are used to create `Axes`
-    objects in the output dictionary `axs`.
+    corresponding dimensions. Any `geo.Box` primitives prefixed with 'Axes' are
+    used to create `Axes` objects in the output dictionary `axs`.
 
     Parameters
     ----------
     prims: LabelledList[geo.Primitive]
-        A list of `Primitive` objects.
-        `Figure` and `Axes` objects are created from objects prefixed with
-        'Figure' or 'Axes'.
+        A list of `Primitive` objects
+
+        `Figure` and `Axes` objects are created from primitives with labels
+        prefixed by 'Figure' or 'Axes'.
 
     Returns
     -------
     fig, axs: typ.Tuple[Figure, typ.Mapping[str, Axes]]
         A `Figure` instance and a mapping from axes labels to `Axes` instances
-        using the `Axes` object names.
+        using the `Axes` object names
     """
 
     width, height = width_and_height_from_box(prims['Figure'])
@@ -76,10 +76,10 @@ def rect_from_box(
         fig_size: typ.Optional[typ.Tuple[float, float]]=(1, 1)
     ) -> typ.Tuple[float, float, float, float]:
     """
-    Return a `rect` tuple, `(left, bottom, width, height)`, of a `Box`
+    Return a `rect` tuple, `(left, bottom, width, height)`, from a `Box`
 
-    This tuple of box information can be used to create a `Bbox` object from
-    `matplotlib` or an `Axes`.
+    This tuple of box information can be used to create a `Bbox` or `Axes`
+    object from `matplotlib`.
 
     Parameters
     ----------
