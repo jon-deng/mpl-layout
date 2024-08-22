08f8efd3214e5ad15969b04540eacd8787223c7a
Jonathan Deng <jonathan.j.deng@gmail.com>
2024-08-22 11:51:46 -0400

    maint: Remove extra imports from `geometry`

--- mpllayout/geometry.py
+++ mpllayout/geometry.py
@@ -1,21 +1,6 @@
 """
-Geometric primitive and constraints
+Geometric primitives and constraints
 """
-
-import typing as typ
-from numpy.typing import NDArray
-
-import numpy as np
-import jax.numpy as jnp
-
-from .array import LabelledTuple
-
-PrimList = typ.Tuple['Primitive', ...]
-ConstraintList = typ.List['Constraint']
-Idxs = typ.Tuple[int]
-
-ArrayShape = typ.Tuple[int, ...]
-PrimTuple = typ.Tuple['Primitive', ...]
 
 from .primitives import *
 from .constraints import *
