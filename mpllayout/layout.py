e36f1dff57de05eab702fa79cbec51837f8fa1d1
Jonathan Deng <jonathan.j.deng@gmail.com>
2024-09-17 17:17:10 -0400

    wip: Replace `PrimitiveTree` with a `Node` for storing primitives
    This simplifies the `Layout` class

--- mpllayout/layout.py
+++ mpllayout/layout.py
@@ -3,13 +3,12 @@ Classes for handling collections of primitives and constraints
 
 The strategy to solve a collection of constraints is to use three labelled
 lists representing a system of non-linear equations:
-    `primitives: PrimLabelledList`
-        A list of `geo.Primitive` instances representing the unknowns of the
-        non-linear equations
+    `primitives: `
+        A collection of `geo.Primitive` instances
 
-        Each `geo.Primitive.param' attribute represents the unknown(s) that must
-        be solved for to satisfy the constraints.
-    `constraints: ConstraintLabelledList`
+        These represent the unknowns parameters of the geometric system and is
+        represented as a tree.
+    `constraints: typ.List[geo.Constraint]`
         A list of `geo.Constraint` instances representing the non-linear
         equations
 
@@ -19,8 +18,8 @@ lists representing a system of non-linear equations:
         A graph linking each constraint to the primitives in `primitives`
         (through string labels) the constraint applies to
 
-The class `Layout` handles construction of these three lists while functions
-`solve` and `solve_linear` use these lists to solve the system of constraints.
+The class `Layout` handles construction of these three things while functions
+`solve` and `solve_linear` use the layout to solve the system of constraints.
 """
 
 import typing as typ
@@ -28,184 +27,12 @@ import typing as typ
 import numpy as np
 
 from . import geometry as geo
-from .containers import LabelledList
+from .containers import Node
 
-Prim = geo.Primitive
-PrimLabelledList = LabelledList[geo.Primitive]
-ConstraintLabelledList = LabelledList[geo.Constraint]
+Primitive = geo.Primitive
 
 IntGraph = typ.List[typ.Tuple[int, ...]]
 StrGraph = typ.List[typ.Tuple[str, ...]]
-
-
-class PrimitiveTree:
-    """
-    Tree structure mapping keys to `Primitive`s
-
-    The tree structure reflects the `Primitive` structure where a primitive can have
-    associated child primitives.
-
-    Parameters
-    ----------
-    data:
-        A `Primitive` associated with the node
-    children:
-        Any child `Primitive` of the primitive
-
-        This follows the recursive layout of `Primitive`. For example, consider a `Line`
-        instance `line` which has two points. The tree representation of `line` is
-        ```
-        PrimitiveTree(line, {'Point0': line['Point0'], 'Point1': line['Point1']})
-        ```
-    """
-
-    def __init__(
-        self, data: typ.Union[None, Prim], children: typ.Mapping[str, "PrimitiveTree"]
-    ):
-        self._data = data
-        self._children = children
-
-    def prim_graph(self) -> typ.Mapping[Prim, int]:
-        """
-        Return a mapping from primitives to integer indices in `self.prims()`
-        """
-        _graph = {tree.data: None for tree in self.values(flat=True)}
-        return {prim: ii for ii, prim in enumerate(_graph)}
-
-    def prims(self) -> typ.List[Prim]:
-        """
-        Return a list of all unique primitives in the tree
-        """
-        return list(self.prim_graph().keys())
-
-    @property
-    def children(self):
-        """
-        Return any child primitives of the primitive in `self.data`
-        """
-        return self._children
-
-    @property
-    def data(self):
-        """
-        Return the associated primitive
-        """
-        return self._data
-
-    ## Dict-like interface
-    def keys(self, flat: bool = False) -> typ.List[str]:
-        """
-        Return child keys
-
-        Parameters
-        ----------
-        flat:
-            Toggle whether to recursively flatten keys
-
-            Child keys are separated using '/'
-        """
-        if flat:
-            flat_keys = []
-            for key, child_tree in self.children.items():
-                flat_keys += [f"{key}"]
-                flat_keys += [
-                    f"{key}/{child_key}" for child_key in child_tree.keys(flat)
-                ]
-            return flat_keys
-        else:
-            return list(self.children.keys())
-
-    def values(self, flat: bool = False) -> typ.List["PrimitiveTree"]:
-        """
-        Return child primitives
-
-        Parameters
-        ----------
-        flat:
-            Toggle whether to recursively flatten child primitives
-        """
-        if flat:
-            flat_values = []
-            for key, child_tree in self.children.items():
-                flat_values += [child_tree]
-                flat_values += child_tree.values(flat)
-            return flat_values
-        else:
-            return list(self.children.values())
-
-    def items(self, flat: bool = False) -> typ.List[typ.Tuple[str, "PrimitiveTree"]]:
-        """
-        Return paired child keys and associated trees
-
-        Parameters
-        ----------
-        flat:
-            Toggle whether to recursively flatten keys and trees
-        """
-
-        return zip(self.keys(flat), self.values(flat))
-
-    def __getitem__(self, key: str):
-        """
-        Return the primitive indexed by a slash-separated key
-
-        Parameters
-        ----------
-        key: str
-            A slash-separated key, for example 'Box/Line0/Point2'
-        """
-        split_key = key.split("/")
-        parent_key = split_key[0]
-        child_key = "/".join(split_key[1:])
-
-        try:
-            if len(split_key) == 1:
-                return self.children[parent_key].data
-            else:
-                return self.children[parent_key][child_key]
-        except KeyError as err:
-            raise KeyError(f"{key}") from err
-
-    def __setitem__(self, key: str, value: "PrimitiveTree"):
-        """
-        Add a primitive indexed by a slash-separated key
-
-        Parameters
-        ----------
-        key: str
-            A slash-separated key, for example 'Box/Line0/Point2'
-        """
-        split_key = key.split("/")
-        parent_key = split_key[0]
-        child_keys = split_key[1:]
-        child_key = "/".join(child_keys)
-
-        try:
-            if len(child_keys) > 0:
-                self.children[parent_key][child_key] = value
-            elif len(child_keys) == 0:
-                self.children[parent_key] = value
-            else:
-                assert False
-
-        except KeyError as err:
-            raise KeyError(f"{key}") from err
-
-    def __len__(self):
-        return len(self.data)
-
-
-def convert_prim_to_tree(prim: Prim) -> PrimitiveTree:
-    """
-    Return a `PrimitiveTree` representation of a `Primitive`
-    """
-    # Recursively create any child trees:
-    children = {
-        child_key: convert_prim_to_tree(child_prim)
-        for child_key, child_prim in prim.prims.items()
-    }
-    return PrimitiveTree(prim, children)
-
 
 class Layout:
     """
@@ -217,8 +44,8 @@ class Layout:
 
     Parameters
     ----------
-    prim_tree:
-        A `PrimitiveTree`
+    prims:
+        A `Node` container of `Primitive`s
     constraints:
         A list of constraints
     constraint_graph:
@@ -227,19 +54,19 @@ class Layout:
 
     def __init__(
         self,
-        prim_tree: typ.Optional[PrimitiveTree] = None,
+        prims: typ.Optional[Node] = None,
         constraints: typ.Optional[typ.List[geo.Constraint]] = None,
         constraint_graph: typ.Optional[StrGraph] = None,
     ):
 
-        if prim_tree is None:
-            prim_tree = PrimitiveTree(None, {})
+        if prims is None:
+            prims = Node(np.array([]), [], [])
         if constraints is None:
             constraints = []
         if constraint_graph is None:
             constraint_graph = []
 
-        self._primitive_tree = prim_tree
+        self._prims = prims
         self._constraints = constraints
         self._constraint_graph = constraint_graph
 
@@ -249,12 +76,9 @@ class Layout:
         self._constraint_type_count = {}
         self._label_to_constraintidx = {}
 
-    def prims(self):
-        return self.prim_tree.prims()
-
     @property
-    def prim_tree(self):
-        return self._primitive_tree
+    def prims(self):
+        return self._prims
 
     @property
     def constraints(self):
@@ -264,19 +88,11 @@ class Layout:
     def constraint_graph(self) -> StrGraph:
         return self._constraint_graph
 
-    @property
-    def constraint_graph_int(self) -> IntGraph:
-        prim_graph = self.prim_tree.prim_graph()
-        return [
-            tuple(prim_graph[self.prim_tree[prim_label]] for prim_label in prim_labels)
-            for prim_labels in self.constraint_graph
-        ]
-
-    def add_prim(self, prim: geo.Primitive, label: typ.Optional[str] = None) -> str:
+    def add_prim(self, prim: geo.Primitive, key: typ.Optional[str] = None) -> str:
         """
         Add a `Primitive` to the `Layout`
 
-        The primitive will be added `self.prim_tree` under the given `label`.
+        The primitive will be added `self.prims` under the given `label`.
 
         Parameters
         ----------
@@ -293,14 +109,14 @@ class Layout:
         label: str
             The label for the added primitive
         """
-        self.prim_tree[label] = convert_prim_to_tree(prim)
-        return label
+        self.prims.add_child(key, prim)
+        return key
 
     def add_constraint(
         self,
         constraint: geo.Constraint,
         prim_labels: typ.Tuple[str, ...]
-    ) -> str:
+    ) -> None:
         """
         Add a `Constraint` between `Primitive`s
 
@@ -308,27 +124,38 @@ class Layout:
         ----------
         constraint:
             The constraint to apply
-        prim_idxs:
-            A tuple of strings referencing primitives (`self.prim_tree`) to apply the constraint
-        constraint_label: typ.Optional[str]
-            An optional label for the constraint.
-            If not provided, an automatic name based on the constraint class
-            will be used.
-
-        Returns
-        -------
-        constraint_label: str
-            The label for the added constraint
+        prim_labels:
+            A tuple of strings referencing primitives (`self.prims`) to apply the constraint
         """
         self.constraints.append(constraint)
         self.constraint_graph.append(prim_labels)
 
+def build_constraint_graph_int(self) -> IntGraph:
+    prim_graph = self.prims.prim_graph()
+    return [
+        tuple(prim_graph[self.prims[prim_label]] for prim_label in prim_labels)
+        for prim_labels in self.constraint_graph
+    ]
+
+def build_prim_graph(prim) -> typ.Mapping[Primitive, int]:
+    """
+    Return a mapping from primitives to integer indices in `self.prims()`
+    """
+    _graph = {tree.value: None for tree in prim.values(flat=True)}
+    return {prim: ii for ii, prim in enumerate(_graph)}
+
+def prims(self) -> typ.List[Primitive]:
+    """
+    Return a list of all unique primitives in the tree
+    """
+    return list(self.prim_graph().keys())
+
 def build_tree(
-    tree: PrimitiveTree,
-    prim_to_idx: typ.Mapping[Prim, int],
+    prim: Primitive,
+    prim_to_idx: typ.Mapping[Primitive, int],
     params: typ.List[np.typing.NDArray],
-    prim_to_newprim: typ.Mapping[Prim, Prim],
-) -> PrimitiveTree:
+    prim_to_newprim: typ.Mapping[Primitive, Primitive],
+) -> Primitive:
     """
     Return a new `PrimitiveTree` using new primitives for given parameter values
 
@@ -355,16 +182,16 @@ def build_tree(
     PrimitiveTree
         The new `PrimitiveTree` with parameters from `params`
     """
-    oldprim = tree.data
+    oldprim = prim.value
 
     # If the tree has no children, we simply create the primitive with no children
     # If the tree has children, we need to recursively create all child trees
-    if len(tree.children) == 0:
+    if len(prim.children) == 0:
         children = {}
     else:
         children = {
             key: build_tree(childtree, prim_to_idx, params, prim_to_newprim)
-            for key, childtree in tree.children.items()
+            for key, childtree in prim.children.items()
         }
 
     if oldprim is None:
@@ -374,8 +201,8 @@ def build_tree(
     else:
         param = params[prim_to_idx[oldprim]]
         newprim = type(oldprim)(
-            param, tuple(child_tree.data for child_tree in children.values())
+            param, tuple(child_tree.value for child_tree in children.values())
         )
         prim_to_newprim[oldprim] = newprim
 
-    return PrimitiveTree(newprim, children)
+    return Primitive(newprim, children)
--- tests/test_layout.py
+++ tests/test_layout.py
@@ -8,63 +8,56 @@ from pprint import pprint
 
 import numpy as np
 
-from mpllayout import geometry as geo, layout as lat
+from mpllayout import geometry as geo, layout as lat, containers
 
 
 class TestPrimitiveTree:
 
     @pytest.fixture()
-    def prim_tree(self):
-        return lat.PrimitiveTree(None, {})
+    def prims(self):
+        return containers.Node(None, [], [])
 
-    def test_set_prim(self, prim_tree):
-        prim_tree["MyBox"] = lat.convert_prim_to_tree(geo.Quadrilateral())
+    def test_set_prim(self, prims):
+        prims.add_child("MyBox", geo.Quadrilateral())
 
         pprint(f"Keys:")
-        pprint(prim_tree.children["MyBox"].keys())
+        pprint(prims["MyBox"].keys())
 
-        pprint("Flat keys:")
-        pprint(prim_tree.keys(flat=True))
-
-        pprint("Unique prims:")
-        pprint(prim_tree.prims())
-
-    def test_build_primtree(self, prim_tree):
+    def test_build_primtree(self, prims):
         point_a = geo.Point([0, 0])
         point_b = geo.Point([1, 1])
-        prim_tree["PointA"] = lat.convert_prim_to_tree(point_a)
-        prim_tree["LineA"] = lat.convert_prim_to_tree(geo.Line([], (point_a, point_b)))
+        prims.add_child("PointA", point_a)
+        prims.add_child("LineA", geo.Line([], (point_a, point_b)))
+        prims.add_child("MySpecialBox", geo.Quadrilateral())
 
-        prim_tree["MySpecialBox"] = lat.convert_prim_to_tree(geo.Quadrilateral())
+        # prim_graph = prim_tree.prim_graph()
 
-        prim_graph = prim_tree.prim_graph()
+        # rng = np.random.default_rng()
 
-        rng = np.random.default_rng()
+        # new_params = [rng.random(prim.param.shape) for prim in prim_tree.prims()]
 
-        new_params = [rng.random(prim.param.shape) for prim in prim_tree.prims()]
+        # new_tree = lat.build_tree(prim_tree, prim_graph, new_params, {})
 
-        new_tree = lat.build_tree(prim_tree, prim_graph, new_params, {})
+        # print("Old primitive graph:")
+        # pprint(prim_tree.prim_graph())
 
-        print("Old primitive graph:")
-        pprint(prim_tree.prim_graph())
+        # print("Old primitive list")
+        # pprint(prim_tree.prims())
 
-        print("Old primitive list")
-        pprint(prim_tree.prims())
+        # print("Old primitive keys")
+        # pprint(prim_tree.keys(flat=True))
 
-        print("Old primitive keys")
-        pprint(prim_tree.keys(flat=True))
+        # print("New parameters")
+        # pprint(new_params)
 
-        print("New parameters")
-        pprint(new_params)
+        # print("New primitive graph:")
+        # pprint(new_tree.prim_graph())
 
-        print("New primitive graph:")
-        pprint(new_tree.prim_graph())
+        # print("New primitive list")
+        # pprint(new_tree.prims())
 
-        print("New primitive list")
-        pprint(new_tree.prims())
-
-        print("New primitive keys")
-        pprint(new_tree.keys(flat=True))
+        # print("New primitive keys")
+        # pprint(new_tree.keys(flat=True))
 
 
 class TestLayout:
@@ -77,7 +70,7 @@ class TestLayout:
 
         layout.add_constraint(geo.PointLocation((0, 0)), ("MyBox/Line0/Point0",))
 
-        pprint(layout.prims())
+        pprint(layout.prims)
         pprint(layout.constraints)
         pprint(layout.constraint_graph)
-        pprint(layout.constraint_graph_int)
+        # pprint(layout.constraint_graph_int)
