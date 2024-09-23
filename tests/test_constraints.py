1e9fe02e128bee53ee6c4d3beabfc6da778754e0
Jonathan Deng <jonathan.j.deng@gmail.com>
2024-09-23 10:39:13 -0400

    misc: Move primitive/constraint tests to separate files

--- tests/test_constraints.py
+++ tests/test_constraints.py
@@ -1,5 +1,5 @@
 """
-Test geometric primitive and constraints
+Test geometric onstraints
 """
 
 import pytest
@@ -12,43 +12,6 @@ from pprint import pprint
 import numpy as np
 
 from mpllayout import geometry as geo
-
-
-class TestPrimitives:
-
-    def test_Quadrilateral(self):
-        quad = geo.Quadrilateral()
-
-    def test_Line(self):
-        line = geo.Line()
-
-    def test_Point(self):
-        point = geo.Point()
-
-    def test_Polygon(self):
-        poly = geo.Polygon()
-
-    def test_Primitive_jax_pytree(self):
-        # breakpoint()
-        from jax import tree_util
-
-        point = geo.Point()
-        line = geo.Line()
-        quad = geo.Quadrilateral()
-
-        for prim in (point, line, quad):
-            print(f"\nTesting primitive type {type(prim).__name__}")
-            leaves = tree_util.tree_leaves(prim)
-            print("Leaves:", leaves)
-
-            value_flat, value_tree = tree_util.tree_flatten(prim)
-            reconstructed_prim = tree_util.tree_unflatten(value_tree, value_flat)
-            print("tree_util.tree_flatten:", value_flat, value_tree)
-            print("tree_util.tree_unflatten:", reconstructed_prim)
-
-            leaves = tree_util.tree_leaves([0, 1, 2, 3, [4, 5, [6, 7, [8]]]])
-            print(leaves)
-            print([type(leaf) for leaf in leaves])
 
 
 class TestConstraints:
