637f5295ceb51ee8ee6eb193f3fa7e76c347e119
Jonathan Deng <jonathan.j.deng@gmail.com>
2024-09-24 11:51:08 -0400

    tests: Refactor `Quadrilateral` test fixtures

--- tests/test_constraints.py
+++ tests/test_constraints.py
@@ -63,6 +63,64 @@ class GeometryFixtures:
         )
 
     ## Quadrilaterals
+    def make_quad(self, translation, deformation):
+
+        # Specify vertices of a unit square, then deform it and translate it
+        verts = np.array([[0, 0], [1, 0], [1, 1], [0, 1]])
+        verts = np.tensordot(verts, deformation, axes=(-1, -1))
+        verts = verts + translation
+
+        return geo.Quadrilateral(
+            value=[], children=tuple(geo.Point(vert) for vert in verts)
+        )
+
+    def make_quad_grid(
+        self,
+        translation: NDArray,
+        col_margins: NDArray,
+        row_margins: NDArray,
+        col_widths: NDArray,
+        row_heights: NDArray,
+    ):
+        """
+        Return a grid of `Quadrilateral`s with shape (M, N)
+
+        Rows and columns are numbered from top to bottom and left to right, respectively.
+
+        Parameters
+        ----------
+        col_margins, row_margins: NDArray (N-1,), (M-1,)
+            Column and row margins
+        col_widths, col_heights: NDArray (N,), (M,)
+            Column and row dimensions
+        """
+        # Determine translations/transformations needed for each quad
+        col_defs = col_widths[:, None, None] * np.outer([1, 0], [1, 0]) + np.outer(
+            [0, 1], [0, 1]
+        )
+        row_defs = np.outer([1, 0], [1, 0]) + row_heights[:, None, None] * np.outer(
+            [0, 1], [0, 1]
+        )
+
+        cumu_widths = np.cumsum(
+            np.concatenate((translation[[0]], col_widths[:-1] + col_margins))
+        )
+        col_trans = np.stack([np.array([x, 0]) for x in cumu_widths], axis=0)
+        cumu_heights = np.cumsum(
+            np.concatenate((translation[[1]], -row_heights[1:] - row_margins))
+        )
+        row_trans = np.stack([np.array([0, y]) for y in cumu_heights], axis=0)
+
+        row_args = zip(row_trans, row_defs)
+        col_args = zip(col_trans, col_defs)
+
+        quads = [
+            self.make_quad(drow + dcol, row_def @ col_def)
+            for (drow, row_def), (dcol, col_def) in itertools.product(
+                row_args, col_args
+            )
+        ]
+        return quads
 
 
 class TestPointConstraints(GeometryFixtures):
@@ -70,19 +128,20 @@ class TestPointConstraints(GeometryFixtures):
     @pytest.fixture()
     def direction(self):
         vec = np.random.rand(2)
-        return vec/np.linalg.norm(vec)
+        return vec / np.linalg.norm(vec)
 
     @pytest.fixture()
     def distance(self):
         return 2.9
 
     def test_DirectedDistance(
-        self,
-        pointa: geo.Point,
-        distance: float,
-        direction: NDArray
+        self, pointa: geo.Point, distance: float, direction: NDArray
     ):
-        pointb = self.make_point(pointa, distance, direction, )
+        pointb = self.make_point(
+            pointa,
+            distance,
+            direction,
+        )
         constraint = geo.DirectedDistance(distance, direction)
         res = constraint((pointa, pointb))
         assert np.all(np.isclose(res, 0))
@@ -136,64 +195,72 @@ class TestLineConstraints(GeometryFixtures):
         assert np.all(np.isclose(res, 0))
 
 
-class TestQuadConstraints:
+class TestQuadConstraints(GeometryFixtures):
+    # @pytest.fixture(params=[(2, 1)])
     @pytest.fixture(params=[(1, 1), (2, 1), (1, 2), (2, 2)])
-    def shape(self, request):
+    def grid_shape(self, request):
         return request.param
 
     @pytest.fixture()
-    def quads(self, shape):
-        num_quads = int(np.prod(shape))
+    def grid_origin_dimensions(self):
+        # width, height = np.random.rand(2)
+        width, height = 1, 1
+        return (width, height)
 
-        quads = []
-        for ii, jj in itertools.product(*(range(size) for size in shape)):
-            height = 1
-            width = 2
-            height_margin = 0.1
-            width_margin = 0.1
-            origin_topleft = np.array(
-                [ii * (height + height_margin), jj * (width + width_margin)],
-                dtype=float,
-            )
-            points = [
-                origin_topleft - [0, height],
-                origin_topleft - [0, height] + [width, 0],
-                origin_topleft - [0, height] + [width, 0] + [0, height],
-                origin_topleft,
-            ]
-            quads.append(geo.Quadrilateral([], [geo.Point(point) for point in points]))
+    @pytest.fixture()
+    def rel_grid_dimensions(self, grid_shape: tp.Tuple[int, int]):
+        num_row, num_col = grid_shape
 
-        return quads
+        ## Random sizes and margins
+        # widths = np.random.rand(num_col - 1)
+        # col_margins = 0.1*np.random.rand(num_col - 1)
 
-    def test_Box(self, quads: tp.List[geo.Quadrilateral], shape: tp.Tuple[int, ...]):
-        num_row, num_col = shape
+        # heights = np.random.rand(num_row - 1)
+        # row_margins = 0.1*np.random.rand(num_row - 1)
 
-        res = geo.Grid(
-            shape,
-            (num_col - 1) * [0.1],
-            (num_row - 1) * [0.1],
-            (num_col - 1) * [1],
-            (num_row - 1) * [1],
-        )(quads)
+        ## Specific sizes and margins
+        rel_col_widths = 2 * np.ones(num_col - 1)
+        col_margins = 1 * np.ones(num_col - 1)
 
-        print(res)
+        rel_row_heights = 6 * np.ones(num_row - 1)
+        row_margins = 3 * np.ones(num_row - 1)
 
-    def test_Grid(self, quads: tp.List[geo.Quadrilateral], shape: tp.Tuple[int, ...]):
-        num_row, num_col = shape
+        return col_margins, row_margins, rel_col_widths, rel_row_heights
 
-        res = geo.Grid(
-            shape,
-            (num_col - 1) * [0.1],
-            (num_row - 1) * [0.1],
-            (num_col - 1) * [1],
-            (num_row - 1) * [1],
-        )(quads)
+    @pytest.fixture()
+    def quads(
+        self,
+        grid_origin_dimensions: tp.Tuple[float, float],
+        rel_grid_dimensions: tp.Tuple[NDArray, NDArray, NDArray, NDArray],
+    ):
+        origin = np.random.rand(2)
+        origin = np.zeros(2)
 
-        print(res)
+        col_margins, row_margins, rel_col_widths, rel_row_heights = rel_grid_dimensions
 
-    # def test_CoincidentPoint(self, points):
-    #     ans_ref = points[0].value
+        origin_width, origin_height = grid_origin_dimensions
+        col_widths = origin_width * np.concatenate(([1], rel_col_widths))
+        row_heights = origin_height * np.concatenate(([1], rel_row_heights))
 
-    #     dist = geo.PointToPointAbsDistance(0)
-    #     ans_com = dist(points[:2])
-    #     assert np.isclose(ans_ref, ans_com)
+        return self.make_quad_grid(
+            origin, col_margins, row_margins, col_widths, row_heights
+        )
+
+    def test_Grid(
+        self,
+        quads: tp.List[geo.Quadrilateral],
+        grid_shape: tp.Tuple[int, int],
+        rel_grid_dimensions: tp.Tuple[NDArray, NDArray, NDArray, NDArray],
+    ):
+        res = geo.Grid(grid_shape, *rel_grid_dimensions)(quads)
+        assert np.all(np.isclose(res, 0))
+
+    @pytest.fixture()
+    def quad_box(self):
+        translation = np.random.rand(2)
+        deformation = np.diag(np.random.rand(2))
+        return self.make_quad(translation, deformation)
+
+    def test_Box(self, quad_box: geo.Quadrilateral):
+        res = geo.Box()((quad_box,))
+        assert np.all(np.isclose(res, 0))
