5c69ba3dfec5a6d83a7c3232de078a7f7fb351a7
Jonathan Deng <jonathan.j.deng@gmail.com>
2024-12-20 16:37:31 -0500

    misc: Add initial `constructions` module

---
+++ src/mpllayout/constructions.py
@@ -0,0 +1,7 @@
+"""
+Geometric constructions
+
+Constructions are functions that accept primitives and return a vector. For
+example, this could be the coordinates of a point, the angle between two lines,
+or the length of a single line.
+"""
