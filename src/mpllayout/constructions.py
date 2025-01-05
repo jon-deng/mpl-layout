5d2c9a58f8c263d01a058c064c92d61c09e44ae4
Jonathan Deng <jonathan.j.deng@gmail.com>
2025-01-05 14:58:54 -0500

    misc: Add TODO on construction improvements

--- src/mpllayout/constructions.py
+++ src/mpllayout/constructions.py
@@ -6,6 +6,9 @@ For example, this could be the coordinates of a point, the angle between two
 lines, or the length of a single line.
 """
 
+# TODO: Combine constraints that act on x/y directions into single arugment construction
+# TODO: Rename derived constraints as '...Error'
+
 from typing import Callable, Optional, Any, TypeVar, NamedTuple
 from collections.abc import Iterable
 from numpy.typing import NDArray
